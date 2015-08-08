d6e52a34e020b2f34371a7dbcf71c6a71320666b
Jeff Cortese <JCortese86@gmail.com>
2015-08-08 18:00:27 -0500

    hooked up to angular2 and firebase

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,25 @@
-export class Jeff {
-    constructor(){
-        console.log("yo")
-    }
+/// <reference path="../typings/angular2/angular2.d.ts" />
+
+import * as ng from 'angular2/angular2';
+import * as fire from 'lib/AngularFire';
+import PickList from 'pickList/pickList'
+
+@ng.Component({
+  selector: 'app'
+})
+@ng.View({
+  templateUrl: 'app.html',
+  directives: [ng.NgFor, PickList]
+})
+export class App {
+  fire;
+  players:Array<{}>;
+
+  constructor() {
+    //var yo = new PickList();
+    this.fire = new fire.AngularFire("https://rockfordkeeper2015.firebaseio.com/players");
+    this.players = this.fire.asArray().list;
+  }
 }
 
-var thingy = new Jeff();
+ng.bootstrap(App);
---
+++ app/pickList/pickList.ts
@@ -0,0 +1,20 @@
+import * as ng from 'angular2/angular2';
+import * as fire from 'lib/AngularFire';
+
+@ng.Component({
+  selector: 'pick-list'
+})
+@ng.View({
+  templateUrl: 'pickList/pickList.html',
+  directives: [ng.NgFor]
+})
+class PickList {
+  picks: Array<{}>;
+
+  constructor() {
+    var fireRef = new fire.AngularFire("https://rockfordkeeper2015.firebaseio.com/picks");
+    this.picks = fireRef.asArray().list;
+  }
+}
+
+export default PickList

9a186142412e1f77dbb9e835c56549f05b61844a
Jeff Cortese <JCortese86@gmail.com>
2015-08-08 18:54:00 -0500

    some more stuff

--- app/app.ts
+++ app/app.ts
@@ -2,23 +2,22 @@
 
 import * as ng from 'angular2/angular2';
 import * as fire from 'lib/AngularFire';
-import PickList from 'pickList/pickList'
+import PickList from 'pickList/pickList';
+import TeamList from 'teamList/teamList';
 
 @ng.Component({
   selector: 'app'
 })
 @ng.View({
   templateUrl: 'app.html',
-  directives: [ng.NgFor, PickList]
+  directives: [ng.NgFor, PickList, TeamList]
 })
 export class App {
-  fire;
   players:Array<{}>;
 
   constructor() {
-    //var yo = new PickList();
-    this.fire = new fire.AngularFire("https://rockfordkeeper2015.firebaseio.com/players");
-    this.players = this.fire.asArray().list;
+    var fireRef = new fire.AngularFire("https://rockfordkeeper2015.firebaseio.com/players");
+    this.players = fireRef.asArray().list;
   }
 }
 
---
+++ app/teamList/teamList.ts
@@ -0,0 +1,20 @@
+import * as ng from 'angular2/angular2';
+import * as fire from 'lib/AngularFire';
+
+@ng.Component({
+  selector: 'team-list'
+})
+@ng.View({
+  templateUrl: 'teamList/teamList.html',
+  directives: [ng.NgFor]
+})
+class TeamList {
+  teams: Array<{}>;
+
+  constructor() {
+    var fireRef = new fire.AngularFire("https://rockfordkeeper2015.firebaseio.com/teams");
+    this.teams = fireRef.asArray().list;
+  }
+}
+
+export default TeamList
